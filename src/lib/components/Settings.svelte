4b90142fa9c18f948b7c86aa6562adaf2c5c075c
Wessel <wessel01@ziggo.nl>
2022-07-10 14:22:49 +0200

    add settings menu

---
+++ src/lib/components/Settings.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+    import { max } from "../stores";
+
+    export let toggleVisible: () => void;
+</script>
+
+<div class="settings-back">
+    <div class="settings-front">
+        <div class="settings-header">
+            <h2>Settings</h2>
+            <button on:click={toggleVisible}>✖️</button>
+        </div>
+        <div class="settings-content">
+            <div class="row">
+                <h3>Maximum value:</h3>
+                <input type="number" bind:value={$max} />
+            </div>
+            <div class="row">
+                <p>The maximum value will be used in the expressions.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .settings-back {
+        background-color: #06283d80;
+        width: 100vw;
+        height: 100vh;
+        position: absolute;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .settings-front {
+        background-color: white;
+        padding: 1em;
+        padding-inline: 2em;
+        border-radius: 0.5em;
+    }
+
+    .settings-header {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    button {
+        background-color: transparent;
+        border: none;
+        font-size: 1.5em;
+        cursor: pointer;
+
+        transition: transform 0.2s ease-in-out;
+    }
+
+    button:hover {
+        transform: scale(1.2);
+    }
+
+    .settings-content > .row {
+        display: flex;
+        align-items: center;
+        max-width: 60vw;
+    }
+
+    input {
+        border: none;
+        font-size: 1.2em;
+        width: 3em;
+        height: 1.2em;
+        margin-left: 1em;
+        text-align: center;
+        border-bottom: 2px solid #06283d;
+    }
+
+    /* Chrome, Safari, Edge, Opera */
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    /* Firefox */
+    input[type="number"] {
+        -moz-appearance: textfield;
+    }
+
+    input:focus {
+        outline: none;
+    }
+</style>
--- src/lib/services/MathService.ts
+++ src/lib/services/MathService.ts
@@ -1,26 +1,33 @@
 import type Expression from "src/models/Expression";
 import axios from "axios"
 import { ExpressionType } from "../enums/ExpressionType";
+import { max } from "../stores";
+
+let validMax: number;
+const unsubscribe = max.subscribe(value => {
+    if(value < 100 && value > 1) validMax = value;
+	else validMax = 99;
+});
 
 export default class MathService {
     private static getRandomExpression = async () => {
-        return await (await axios.get('https://x-math.herokuapp.com/api/random')).data as Expression;
+        return await (await axios.get('https://x-math.herokuapp.com/api/random' + `?max=${validMax}`)).data as Expression;
     }
 
     private static getAdditionExpression = async () => {
-        return await (await axios.get('https://x-math.herokuapp.com/api/add')).data as Expression;
+        return await (await axios.get('https://x-math.herokuapp.com/api/add' + `?max=${validMax}`)).data as Expression;
     }
 
     private static getSubtractionExpression = async () => {
-        return await (await axios.get('https://x-math.herokuapp.com/api/sub')).data as Expression;
+        return await (await axios.get('https://x-math.herokuapp.com/api/sub' + `?max=${validMax}`)).data as Expression;
     }
 
     private static getMultiplicationExpression = async () => {
-        return await (await axios.get('https://x-math.herokuapp.com/api/mul')).data as Expression;
+        return await (await axios.get('https://x-math.herokuapp.com/api/mul' + `?max=${validMax}`)).data as Expression;
     }
 
     private static getDivisionExpression = async () => {
-        return await (await axios.get('https://x-math.herokuapp.com/api/div')).data as Expression;
+        return await (await axios.get('https://x-math.herokuapp.com/api/div' + `?max=${validMax}`)).data as Expression;
     }
 
     private static parseExpressionFunctionType = (type: ExpressionType) => {
